bea094223b13fc77a75b19b615ac0a5b396770b9
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2023-12-09 14:42:24 +0400

    Add project files.

---
+++ DP_6_Task_3/Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+
+
+public class ClothesShop
+{
+    private double endirimFaizi; 
+    
+    List<Customer> customers = new List<Customer>();
+
+    public double EndirimFaizi
+    {
+        get { return endirimFaizi; } 
+        set { endirimFaizi = value; XeberVer(); } 
+
+    }
+
+    void XeberVer()
+    {
+        foreach (var item in customers)
+        {
+            Console.WriteLine($"{item.Name} mustermiz {endirimFaizi} endirim var");
+        }
+        
+    }
+
+
+    public void addCustomer(Customer customer){customers.Add(customer);}
+
+    public void removeCustomer(Customer customer) { customers.Remove(customer); }
+
+
+
+}
+
+
+
+public class Customer
+{
+    public string Name { get; set; }
+
+
+
+
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ClothesShop clothesShop = new ClothesShop();
+        
+        Customer customer1 = new Customer() { Name="murad" };
+        Customer customer2 = new Customer() { Name="selim" };
+        Customer customer3 = new Customer() { Name="kamal" };
+
+        clothesShop.addCustomer(customer1);
+        clothesShop.addCustomer(customer2);
+        clothesShop.addCustomer(customer3);
+
+
+        clothesShop.EndirimFaizi = 4;
+
+
+        clothesShop.removeCustomer(customer3);
+
+        Console.WriteLine();
+
+        clothesShop.EndirimFaizi = 9;
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
